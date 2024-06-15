e9f55a5f74c5c473f416857b1f6cfe0a2ec76de3
Sade Nesmith <sadework21@gmail.com>
2024-06-14 22:44:53 -0400

    Initial Commit

---
+++ AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by sade on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
